4f88d5abfafbe6c9e48635b31d0d454715fef50d
infolanze807 <techteam.infolanze@gmail.com>
2024-12-26 21:51:45 +0530

    ad...

--- GNO-Frontend/src/Pages/Bitgetbot.js
+++ GNO-Frontend/src/Pages/Bitgetbot.js
@@ -72,7 +72,7 @@ const Bitgetbot = () => {
     const fetchData = async () => {
       setLoading(true);
       try {
-        const response = await axios.get("http://https://reivun-gkdi.vercel.app:5000/api/symbols");
+        const response = await axios.get("https://reivun-gkdi.vercel.app:5000/api/symbols");
         setSymbolsData(response.data);
         console.log(response.data, "get");
       } catch (error) {
